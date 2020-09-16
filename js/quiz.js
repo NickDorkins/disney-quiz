635b7fc06156de822bbbf54179d33a5c594ebd55
Deanna Johnson <deannaj401@gmail.com>
2020-09-16 16:00:05 -0600

    have quiz working sort of

--- js/quiz.js
+++ js/quiz.js
@@ -1,5 +1,6 @@
 'use strict';
 
+
 // we will asign all the QA object that we will need for the quiz
 var quiz = [];
 
@@ -7,7 +8,7 @@ var quiz = [];
 var count = 0;
 
 // Keep track of the current question being rendered
-var currentQuestionIdx = 0;
+// var currentQuestionIdx = 0;
 
 // QA constructor
 var QA = function (question, answerArray, idQuestion, idAnswer) {
@@ -18,6 +19,7 @@ var QA = function (question, answerArray, idQuestion, idAnswer) {
   this.idAnswer = idAnswer;
   quiz.push(this);
 };
+
 
 // this instance method will create a new Answer and then add it to this.answerArray (I do not think i need this anymore because of line 29...)
 QA.prototype.addAnswer = function (answer, point) {
@@ -33,7 +35,8 @@ var Answer = function (answer, point) {
 // This is where we create our questions
 new QA(
   'When I\'m watching a movie I',
-  [new Answer('Hope the Hero wins 100%', 1),
+  [new Answer('<--Choose Option-->', 0),
+    new Answer('Hope the Hero wins 100%', 1),
     new Answer('Secretly root for the villain', 2),
     new Answer('Cheer when the hero wins but feel bad for the villain...they are just misunderstood', 3),
     new Answer('I don\'t have time for movies...I\'m too busy trying to save the universe from evil sith Lords', 4),
@@ -42,7 +45,8 @@ new QA(
   'answer1');
 new QA(
   'My preferred habitat is',
-  [new Answer('English cottage', 1),
+  [new Answer('<--Choose Option-->', 0),
+    new Answer('English cottage', 1),
     new Answer('Gloomy castle', 2),
     new Answer('Pirate ship', 3),
     new Answer('Modern metal and glass building', 4),
@@ -51,7 +55,8 @@ new QA(
   'answer2');
 new QA(
   'My ideal pet would be:',
-  [new Answer('Cat', 1),
+  [new Answer('<--Choose Option-->', 0),
+    new Answer('Cat', 1),
     new Answer('Bunny', 2),
     new Answer('Cricket', 3),
     new Answer('Raven', 4),
@@ -60,7 +65,8 @@ new QA(
   'answer3');
 new QA(
   'The best present I could receive would be:',
-  [new Answer('A book', 1),
+  [new Answer('<--Choose Option-->', 0),
+    new Answer('A book', 1),
     new Answer('A watch', 2),
     new Answer('A sewing machine', 3),
     new Answer('A roll of scotch tape', 4),
@@ -69,13 +75,15 @@ new QA(
   'answer4');
 new QA(
   'When I grow up, I want to be:',
-  [new Answer('A King', 1),
+  [new Answer('<--Choose Option-->', 0),
+    new Answer('A King', 1),
     new Answer('Stinkin\' Rich', 2),
     new Answer('A Super Hero!!!', 3),
     new Answer('Robot/Android', 4),
     new Answer('Lord of the Underworld', 5)],
   'question5',
   'answer5');
+
 
 function renderQuiz() {
   for (var i = 0; i < quiz.length; i++) {
@@ -89,19 +97,22 @@ function renderQuiz() {
       buttomEl.appendChild(answerEl);
     }
   }
+
 }
 
-function processAnswer() {
+// function processAnswer() {
 
-  if (quiz.selectedAnswer !== -1) {
-    var question = quiz[currentQuestionIdx];
-    var answer = question.answerArray[question.selectedAnswer];
-    count += answer.point;
-  }
-}
+//   if (quiz.selectedAnswer !== -1) {
+//     var question = quiz[currentQuestionIdx];
+//     var answer = question.answerArray[question.selectedAnswer];
+//     count += answer.point;
+//   }
+// }
+
 
 // We need to store the answers for the selected answer
 function addSelectedAnswer() {
+
   for (var i = 0; i < quiz.length; i++) {
     var selectElement = document.getElementById(quiz[i].idAnswer);
     var answerValue = selectElement.value;
@@ -112,6 +123,13 @@ function addSelectedAnswer() {
         count += quiz[i].answerArray[j].point;
       }
     }
+
+    // in ls entry: key: score
+    // value: ## (string)
+
+    // add a new item to localstorage with th key of "score" value ## (count)
+    localStorage.setItem('score', count);
+
     console.log(count);
     // var text = quiz[i].answerArray.point;
     // count += parseInt(text, 10);
@@ -128,5 +146,13 @@ function handleSubmit(event) {
 
 // Create event listener to submit quiz answers
 var submitQuiz = document.getElementById('submit');
+
+console.log(submitQuiz);
 submitQuiz.addEventListener('submit', handleSubmit);
 renderQuiz();
+
+
+
+
+
+
